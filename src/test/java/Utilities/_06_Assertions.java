b3590e77ef7da5a52bfc13734d47f974bcd5701e
ROMA2 <romasecure84@yandex.ru>
2024-07-18 00:34:37 +0400

    _06_Assertions class created

---
+++ src/test/java/Utilities/_06_Assertions.java
@@ -0,0 +1,52 @@
+package Utilities;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _06_Assertions{
+    @Test
+    public void equalsExample() {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "Hello1";
+
+        Assert.assertEquals(s1, s2,"equality could not be achieved");
+        Assert.assertEquals(s1, s3,"equality could not be achieved");
+    }
+
+    @Test
+    public void notEqualsExample() {
+        int x = 10;
+        int y = 12;
+        int z = 10;
+
+        Assert.assertNotEquals(x, y, "equality could not be achieved");
+        Assert.assertNotEquals(x, z, "equality could not be achieved");
+    }
+
+    @Test
+    public void trueExample() {
+        String str1 = "Hello world";
+        String str2 = "Hello";
+
+        Assert.assertTrue(str1.contains(str2));
+        Assert.assertTrue(str1.contains("world"));
+    }
+
+    @Test
+    public void nullExample() {
+        String s1 = null;
+        String s2 = "Hello";
+
+        Assert.assertNull(s1, "S1 is not null");
+        Assert.assertNotNull(s2, "S2 is not null");
+    }
+
+    @Test
+    public void directFailExample() {
+        int a = 50;
+        if (a == 50){
+            Assert.fail("condition not met");
+        }
+    }
+}
