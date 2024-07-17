f098e7b9330a151230eb9a1f8e371f2dc00eb92e
ROMA2 <romasecure84@yandex.ru>
2024-07-17 22:36:04 +0400

    BaseDriver class created

---
+++ src/test/java/Utilities/BaseDriver.java
@@ -0,0 +1,43 @@
+package Utilities;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void initialOperations(){
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+
+        Duration dr = Duration.ofSeconds(20);
+        driver.manage().timeouts().pageLoadTimeout(dr);
+        driver.manage().timeouts().implicitlyWait(dr);
+    }
+
+    @AfterClass
+    void finishingOperations(){
+        MyFunctions.WaitFor(5);
+        driver.quit();
+    }
+
+}
