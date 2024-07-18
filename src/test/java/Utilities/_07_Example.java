1d174c6c52e6f66984aa5d10fb0c1a16fa94e65f
ROMA2 <romasecure84@yandex.ru>
2024-07-19 00:21:55 +0400

    _07_Example class created

---
+++ src/test/java/Utilities/_07_Example.java
@@ -0,0 +1,42 @@
+package Utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _07_Example extends BaseDriver{
+    By link = By.linkText("Newsletter");
+
+    By subYes = By.xpath("(//input[@name='newsletter'])[1]");
+
+    By subNo = By.xpath("(//input[@name='newsletter'])[2]");
+
+    By conBtn = By.cssSelector("input[value='Continue']");
+
+    By message = By.xpath("//div[@id='account-account']/div[1]");
+
+    @Test(priority = 1)
+    public void subscribeYes() {
+        WebElement newsletterLink = driver.findElement(link);
+        newsletterLink.click();
+        WebElement subscribeYes = driver.findElement(subYes);
+        subscribeYes.click();
+        WebElement continueBtn = driver.findElement(conBtn);
+        continueBtn.click();
+
+        MyFunctions.successMessageValidation();
+    }
+
+    @Test(priority = 1)
+    public void subscribeNo() {
+        WebElement newsletterLink = driver.findElement(link);
+        newsletterLink.click();
+        WebElement subscribeNo = driver.findElement(subNo);
+        subscribeNo.click();
+        WebElement continueBtn = driver.findElement(conBtn);
+        continueBtn.click();
+
+        MyFunctions.successMessageValidation();
+    }
+}
