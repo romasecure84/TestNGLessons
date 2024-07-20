90de34ccb9c6ab37d488a65416a440e890b00a65
ROMA2 <romasecure84@yandex.ru>
2024-07-20 23:38:40 +0400

    _11_SecondClass class created

---
+++ src/test/java/_11_SecondClass.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class _11_SecondClass {
+    @Test
+    public void SC_Test1() {
+        System.out.println("Second Class Test1");
+    }
+    @Test
+    public void SC_Test2() {
+        System.out.println("Second Class Test2");
+    }
+}
